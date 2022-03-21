d4598838b4a925bbc56e9e9e2716005a3f6b865f
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2022-03-21 12:01:52 -0300

    adicionando funções do codigo

---
+++ src/classe/structure_functions.py
@@ -0,0 +1,12 @@
+class StructureFunction:
+    def inicio():
+        print('*-' * 30)
+        print(f'{" Lista de Compras ":=^60}')
+        print('*-' * 30, '\n')
+
+
+    def nome_da_primeira_lista():
+        nome_primeira_lista = input('Escolha o nome da sua primeira lista:\n').capitalize()
+        return nome_primeira_lista
+
+
