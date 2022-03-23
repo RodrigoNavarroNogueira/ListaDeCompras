63a0d93db3cd5591cb36aa6932d678c7ac63debd
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2022-03-23 18:09:59 -0300

    Adicionando a opção 2 no menu

--- src/classe/main_class.py
+++ src/classe/main_class.py
@@ -1,11 +1,9 @@
 from datetime import datetime
-import random
 
 import sqlite3
 
 from src.classe.list_creator import ListCreator
 from src.classe.structure_functions import StructureFunction
-
 
 funcoes = StructureFunction
 lista = ListCreator
@@ -13,6 +11,16 @@ funcoes.initiation()
 list_name = funcoes.name_list()
 option = funcoes.start()
 
+
 if option == 1:
     funcoes.list_viewer(list_name)
+    funcoes.start()
 
+elif option == 2:
+    print('chegou')
+    funcoes.lista_para_manipular(list_name)
+    add = funcoes.adicionar_ou_remover()
+
+    if add == 'A':
+        print('chegou')
+        #add_produtos()
