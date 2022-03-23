c4376f1cf25e22c93118bff9b6bd22acfbda1f38
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2022-03-22 23:17:17 -0300

    atualizando classe da estrutura do codigo

--- src/classe/structure_functions.py
+++ src/classe/structure_functions.py
@@ -1,12 +1,21 @@
 class StructureFunction:
-    def inicio():
+    def initiation():
         print('*-' * 30)
         print(f'{" Lista de Compras ":=^60}')
         print('*-' * 30, '\n')
 
 
-    def nome_da_primeira_lista():
-        nome_primeira_lista = input('Escolha o nome da sua primeira lista:\n').capitalize()
-        return nome_primeira_lista
+    def name_list():
+        name_list = input('Escolha o nome da sua lista:\n').capitalize()
+        return name_list
 
 
+    def start():
+        option = 0
+        while option == 0 or option not in [1,2,3,4]:
+            option = int(input("""\nSelecione a opção que você deseja: 
+        [ 1 ] - Visualizar Listas 
+        [ 2 ] - Adicionar/Remover Itens na Lista
+        [ 3 ] - Criar/Excluir e Renomear uma Lista
+        [ 4 ] - Fechar o Programa\n\n"""))
+        return option
