57e42c9ba8278ae93ebc932dee6d8c9c7a546a8d
Rodrigo Navarro <rodrigo.navarro36@hotmail.com>
2022-03-23 22:49:08 -0300

    Atualizando main

--- src/classe/main_class.py
+++ src/classe/main_class.py
@@ -1,26 +1,29 @@
-from datetime import datetime
-
-import sqlite3
-
 from src.classe.list_creator import ListCreator
 from src.classe.structure_functions import StructureFunction
+from src.db.concrete.list_engine import ListEngine
 
 funcoes = StructureFunction
 lista = ListCreator
+engine = ListEngine()
 funcoes.initiation()
 list_name = funcoes.name_list()
 option = funcoes.start()
-
 
 if option == 1:
     funcoes.list_viewer(list_name)
     funcoes.start()
 
 elif option == 2:
-    print('chegou')
     funcoes.lista_para_manipular(list_name)
-    add = funcoes.adicionar_ou_remover()
+    escolha = funcoes.adicionar_ou_remover()
 
-    if add == 'A':
-        print('chegou')
-        #add_produtos()
+    if escolha == 'A':
+        funcoes.add_produtos(list_name)
+        funcoes.start()
+
+    elif escolha == 'R':
+        funcoes.remover_produtos()
+        funcoes.start()
+
+    elif escolha == 'U':
+        ...
