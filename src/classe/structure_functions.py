6d579b5251e0ebb3d0ef8c81e268d1ef9b9e4ebf
Rodrigo <rodrigo.navarro36@hotmail.com>
2023-02-09 22:59:36 -0300

    Melhorando o e-mail recebido

--- src/classe/structure_functions.py
+++ src/classe/structure_functions.py
@@ -62,7 +62,7 @@ class StructureFunction:
             result = []
             for produto in x:
                 print(f'Produto: {x[count][1]} / Quantidade: {x[count][2]}\n')
-                result.append(f'Produto: {x[count][1]} / Quantidade: {x[count][2]}')
+                result.append(f'<p>Produto: {x[count][1]} / Quantidade: {x[count][2]}</p>')
                 count += 1
             y = str(result)
             z = y.replace(',', '\n')
